2ddbd6baaafb6957fc8585b1aaf2287d5067b1db
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-01-26 18:38:49 -0800

    deleted nested repo and pushing everything up to main repo

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//when you input the city it has to show the forecast for the next 5 days and add the city to the history.
+
+//when you view the current weather conditions for that city populate city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+// add color to the UV index to show whether the weather is favorable, moderate, or sever (green, orange, red)
+
+//populate the cards for the next 5 days with date, an icon representation of weather conditions, the temperature, and the humidity
+
+//when you click on a city in the history section then it shows current and future conditions for that city again.
