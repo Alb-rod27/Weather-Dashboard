a610f29caf1f488af243fbea55f41857802a6381
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-01-28 20:53:49 -0800

    added input functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,48 @@
-//when you input the city it has to show the forecast for the next 5 days and add the city to the history.
+$(document).ready(function() {
+    $("#start-btn").on("click", function() {
 
-//when you view the current weather conditions for that city populate city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+        var userInput = $("#input-value").val();
+        currentWeather(userInput)
+    })
 
-// add color to the UV index to show whether the weather is favorable, moderate, or sever (green, orange, red)
+    //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    var apiKey = "7ca2dd2d0abe48ddd1b123c1a4753c0e"
 
-//populate the cards for the next 5 days with date, an icon representation of weather conditions, the temperature, and the humidity
+    function currentWeather(userInput) {
+        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey;
+      
+        // make a get request to url
+        fetch(queryUrl)
+          .then(function(response) {
+            
+              return response.json();
+            // request was successful
+            
+          })
+          .then(function(data) {
+              var currentWeatherEl = $("#current-weather")
+              currentWeatherEl.empty();
+              console.log(data)
+            var cardContainer = $("<div>").addClass("card")
+            var cardBody = $("<div>").addClass("card-body")
+            var cityName = $("<h1>").addClass("card-title").text(data.name)
+            
+            cardBody.append(cityName)
+            cardContainer.append(cardBody)
+            currentWeatherEl.append(cardContainer)
 
-//when you click on a city in the history section then it shows current and future conditions for that city again.
+
+    })
+    };
+    //when you click on a city in the history section then it shows current and future conditions for that city again.
+    
+    //when you input the city it has to show the forecast for the next 5 days and add the city to the history.
+    
+    //when you view the current weather conditions for that city populate city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+    
+    // add color to the UV index to show whether the weather is favorable, moderate, or sever (green, orange, red)
+    
+    //populate the cards for the next 5 days with date, an icon representation of weather conditions, the temperature, and the humidity
+    
+    
+})
