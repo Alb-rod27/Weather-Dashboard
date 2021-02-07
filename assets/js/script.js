d48dd03302101e49d37b806324db515c445ba0cb
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-02-07 10:00:57 -0800

    made sure weather generates

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,19 +35,17 @@ $(document).ready(function() {
     })
     
     var saveTasks = function() {
-      localStorage.setItem("currentWeather", JSON.stringify(currentWeather));
+      console.log(userInput)
+      localStorage.setItem("currentWeather", JSON.stringify(userInput));
     }
 
+    var loadTasks = function() {
+      tasks = JSON.parse(localStorage.getItem("userInput"));
+
     };
-    //when you click on a city in the history section then it shows current and future conditions for that city again.
     
-    //when you input the city it has to show the forecast for the next 5 days and add the city to the history.
-    
-    //when you view the current weather conditions for that city populate city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-    
-    // add color to the UV index to show whether the weather is favorable, moderate, or sever (green, orange, red)
-    
-    //populate the cards for the next 5 days with date, an icon representation of weather conditions, the temperature, and the humidity
-    
-    
+    loadTasks();
+};
+
 })
+
